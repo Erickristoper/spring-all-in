38a39fed555870f661720181858a859a10554b86
Eric Del Socorro <erickdelsocorro@gmail.com>
2024-11-20 22:55:37 +0800

    thymeleaf template engine. Use of Model class

--- fight/src/main/java/com/delsocorro/fight/controllers/MainController.java
+++ fight/src/main/java/com/delsocorro/fight/controllers/MainController.java
@@ -1,14 +1,18 @@
 package com.delsocorro.fight.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/homez")
-    public String requestMethodName() {
+    @RequestMapping("/home")
+    public String home(Model page) {
+        page.addAttribute("username", "Tawanchai");
+        page.addAttribute("attack", "left elbow");
+        page.addAttribute("color", "red");
         return "homez.html";
     }
     

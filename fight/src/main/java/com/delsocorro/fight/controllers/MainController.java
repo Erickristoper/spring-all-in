b3cb33575c2261142bb535b33990761b0345e963
Eric Del Socorro <erickdelsocorro@gmail.com>
2024-11-21 23:30:13 +0800

    @RequestParam(required=false)
    using http request parameter

--- fight/src/main/java/com/delsocorro/fight/controllers/MainController.java
+++ fight/src/main/java/com/delsocorro/fight/controllers/MainController.java
@@ -3,6 +3,9 @@ package com.delsocorro.fight.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -15,5 +18,14 @@ public class MainController {
         page.addAttribute("color", "red");
         return "homez.html";
     }
+
+    @RequestMapping("/requestparam")  // requestparam?name=musemechi
+    public String requestParam(@RequestParam String name, @RequestParam(required=false) String color, Model page) {
+        page.addAttribute("username", name);
+        page.addAttribute("attack", "left elbow");
+        page.addAttribute("color", color);
+        return "homez.html";
+    }
+    
     
 }

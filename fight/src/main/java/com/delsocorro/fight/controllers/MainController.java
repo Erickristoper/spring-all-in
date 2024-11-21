3dfba3b2774c5dae507634ef25e73c5fc15dbb8b
Eric Del Socorro <erickdelsocorro@gmail.com>
2024-11-21 23:36:57 +0800

    @PathVariable to get client data
    /home/{name}

--- fight/src/main/java/com/delsocorro/fight/controllers/MainController.java
+++ fight/src/main/java/com/delsocorro/fight/controllers/MainController.java
@@ -2,6 +2,7 @@ package com.delsocorro.fight.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,21 +12,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
 
-    @RequestMapping("/home")
-    public String home(Model page) {
-        page.addAttribute("username", "Tawanchai");
-        page.addAttribute("attack", "left elbow");
-        page.addAttribute("color", "red");
-        return "homez.html";
-    }
-
-    @RequestMapping("/requestparam")  // requestparam?name=musemechi
-    public String requestParam(@RequestParam String name, @RequestParam(required=false) String color, Model page) {
+    @RequestMapping("/home/{name}/{color}")
+    public String home(@PathVariable String name, @PathVariable String color, Model page) {
         page.addAttribute("username", name);
         page.addAttribute("attack", "left elbow");
         page.addAttribute("color", color);
         return "homez.html";
     }
+
     
     
 }

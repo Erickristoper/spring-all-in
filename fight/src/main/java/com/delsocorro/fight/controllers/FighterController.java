2e35b2909f01016dd9d43bafac760d36db495cbe
Eric Del Socorro <erickdelsocorro@gmail.com>
2024-11-24 22:47:00 +0800

    returning a collection of objects

--- fight/src/main/java/com/delsocorro/fight/controllers/FighterController.java
+++ fight/src/main/java/com/delsocorro/fight/controllers/FighterController.java
@@ -1,5 +1,9 @@
 package com.delsocorro.fight.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,5 +29,32 @@ public class FighterController {
                                 "Legendary boxer and social activist."
                                 );
     }
+
+    @GetMapping("/fighters")
+    public List<CombatFighter> getFighters() {
+        return Arrays.asList(
+                    new CombatFighter(
+                                1L,
+                                "Muhammad Ali",
+                                "The Greatest",
+                                "Heavyweight",
+                                "Boxing",
+                                56,
+                                5,
+                                0,
+                                "Legendary boxer and social activist."
+                                ),
+                    new CombatFighter(
+                                2L,
+                                "Conor McGregor",
+                                "The Notorious",
+                                "Lightweight",
+                                "Mixed Martial Arts",
+                                21,
+                                6,
+                                0,
+                                "Started as a boxer and moved to MMA"));
+    }
+    
     
 }

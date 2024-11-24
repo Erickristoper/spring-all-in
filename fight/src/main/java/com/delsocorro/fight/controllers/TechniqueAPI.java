62b6720cb701f7ad5db552ed2baa2b2a0c295ecb
Eric Del Socorro <erickdelsocorro@gmail.com>
2024-11-24 22:29:49 +0800

    Using @RestController istead of @Response Biody

--- fight/src/main/java/com/delsocorro/fight/controllers/TechniqueAPI.java
+++ fight/src/main/java/com/delsocorro/fight/controllers/TechniqueAPI.java
@@ -1,17 +1,21 @@
 package com.delsocorro.fight.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class TechniqueAPI {
 
     @GetMapping("/technique")
-    @ResponseBody
     public String technique() {
         return "Jab";
     }
+
+    @GetMapping("/kumusta")
+    public String kumusta() {
+        return "Kumusta ka!";
+    }
+
     
 }

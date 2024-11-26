9f68105075a00c0b197122e6a212bcf91d5e42dc
Eric Del Socorro <erickdelsocorro@gmail.com>
2024-11-27 00:05:11 +0800

    setting up the backend

--- Muay/src/main/java/dev/ericds/Muay/controllers/TechniqueAPI.java
+++ Muay/src/main/java/dev/ericds/Muay/controllers/TechniqueAPI.java
@@ -1,15 +1,33 @@
 package dev.ericds.Muay.controllers;
 
+import java.util.UUID;
+import java.util.logging.Logger;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import dev.ericds.Muay.model.Technique;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 
 @RestController
 public class TechniqueAPI {
 
-    @GetMapping("/technique")
-    public String technique() {
-        return "Jab";
+    Logger log = Logger.getLogger(TechniqueAPI.class.getName());
+
+    @PostMapping("/attack")
+    public ResponseEntity<Technique> attack(
+        @RequestHeader String requestId,
+        @RequestBody Technique t
+    ) {
+        log.info("Received an attack of ->  " + t.getName() + " with request id of " + requestId);
+        t.setId(UUID.randomUUID().toString());
+
+        return ResponseEntity.status(HttpStatus.OK).header("requestId", requestId).body(t);
     }
 
     @GetMapping("/kumusta")
